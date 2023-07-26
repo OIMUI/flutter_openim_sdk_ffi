7fb2f76253293739e2f07987bc89c37e9d7e9410
Spicely <Spicely@outlook.com>
2023-07-26 14:41:42 +0800

    修复ios oc找不到文件问题

--- ios/Classes/listener/OnConnListener.swift
+++ ios/Classes/listener/OnConnListener.swift
@@ -1,37 +1,37 @@
 
 
-public protocol OnConnListener {
+@objc public protocol OnConnListener {
     /**
      * 连接服务器失败
      * 可以提示用户当前网络连接不可用
      */
-    func onConnectFailed(code: Int64, error: String)
-
+    @objc func onConnectFailed(code: Int64, error: String)
+    
     /**
      * 已经成功连接到服务器
      */
-    func onConnectSuccess()
-
+    @objc  func onConnectSuccess()
+    
     /**
      * 正在连接到服务器
      * 适合在 UI 上展示“正在连接”状态。
      */
-    func onConnecting()
-
+    @objc func onConnecting()
+    
     /**
      * 当前用户被踢下线
      * 此时可以 UI 提示用户“您已经在其他端登录了当前账号，是否重新登录？”
      */
-    func onKickedOffline()
-
+    @objc  func onKickedOffline()
+    
     /**
      * 登录票据已经过期
      * 请使用新签发的 UserSig 进行登录。
      */
-    func onUserTokenExpired()
-
+    @objc func onUserTokenExpired()
+    
     /**
      * 应用已经初始化
      */
-    func onInitSDK()
+    @objc func onInitSDK()
 }
